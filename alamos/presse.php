e34072d1ef52b8f96e238ba917622d3be830d12b
Tim Frenzel <tim.frenzel@sva.de>
2024-08-14 15:06:00 +0200

    Neue Schleifen eingefügt & Get-Logging aktiviert.

--- alamos/presse.php
+++ alamos/presse.php
@@ -6,6 +6,20 @@ require_once 'config.php';
 /*
 Telegram Gruppe Presse
 */
+// Log-Datei definieren
+$log_file = 'get_log.txt';
+
+// Aktuelles Datum und Uhrzeit erfassen
+$timestamp = date("Y-m-d H:i:s");
+
+// Alle GET-Parameter erfassen
+$get_params = print_r($_GET, true);
+
+// Log-Nachricht erstellen
+$log_message = "[$timestamp] GET-Parameters: " . $get_params . PHP_EOL;
+
+// In die Log-Datei schreiben
+file_put_contents($log_file, $log_message, FILE_APPEND);
 
 
 //Wenn API Key per POST kommt
@@ -14,11 +28,11 @@ if (isset($_GET['apikey'])) {
 
         //Array mit den GET Infos erstellen
         $EinsatzInfos = [
-            "date" => "Datum: " . $_GET['date'],
-            "einheit" => "Einheit: " . $_GET['einheit'],
-            "ziel" => "Adresse: " . $_GET['ziel'],
-            "keyword" => "Stichwort: #" . $_GET['keyword'],
-            "keywordDescription" => "Beschreibung: " . $_GET['keywordDescription'],
+            "date"      => "Datum: " . $_GET['date'],
+            "einheit"   => "Einheit: " . $_GET['einheit'],
+            "ziel"      => "Adresse: " . $_GET['ziel'],
+            "keyword"   => "Stichwort: #" . $_GET['keywordDescription'],
+            "message"   => "Beschreibung: " . $_GET['message'],
         ];
 
         //Text imploden für Telegram
@@ -30,21 +44,32 @@ if (isset($_GET['apikey'])) {
         //An Alle senden
         telegramApiPresse($PresseChatID, $PresseBotID, $threadIDAlle, $text);
 
+        // Mapping der Schlüsselwörter zu den entsprechenden Thread-IDs
+        $mapping = [
+            'Geilenkausen'      => $threadIDGeilenkausen,
+            'Waldbröl'          => $threadIDWaldbroel,
+            'Thierseifen'       => $threadIDThierseifen,
+            'Heide'             => $threadIDHeide,
+            'Wehrführer'        => $threadIDWehrfuehrer,
+            '11 F Gerätewarte'  => $threadIDHauptamtliche,
+            '11 F B-Dienst'     => $threadIDHauptamtliche,
+            '11 F C-Dienst'     => $threadIDHauptamtliche,
+            '11 F D-Dienst'     => $threadIDHauptamtliche,
+            '11 F FEL Waldbröl' => $threadIDHauptamtliche,
+            '11 F Waldbröl ELW' => $threadIDHauptamtliche
+        ];
 
-        //In die Threads versenden
-        if (stristr($EinsatzInfos['einheit'], 'Geilenkausen')) {
-            $threadID = $threadIDGeilenkausen;
-        } elseif (stristr($EinsatzInfos['einheit'], 'Waldbröl')) {
-            $threadID = $threadIDWaldbroel;
-        } elseif (stristr($EinsatzInfos['einheit'], 'Thierseifen')) {
-            $threadID = $threadIDThierseifen;
-        } elseif (stristr($EinsatzInfos['einheit'], 'Heide')) {
-            $threadID = $threadIDHeide;
-        } elseif (stristr($EinsatzInfos['einheit'], 'Wehrführer')) {
-            $threadID = $threadIDWehrfuehrer;
-        } elseif (stristr($EinsatzInfos['einheit'], 'Hauptamtliche Gerätewarte')) {
-            $threadID = $threadIDHauptamtliche;
+        // Standardmäßig Thread-ID auf 1 setzen (General Topic)
+        $threadID = 1;
+
+        // Über das Mapping iterieren und die passende Thread-ID setzen
+        foreach ($mapping as $keyword => $id) {
+            if (stristr($EinsatzInfos['einheit'], $keyword)) {
+                $threadID = $id;
+                break;
+            }
         }
+
 
         //Nachricht Senden
         $result = telegramApiPresse($PresseChatID, $PresseBotID, $threadID, $text);
@@ -58,7 +83,6 @@ if (isset($_GET['apikey'])) {
         if (isset($_GET['lat']) && isset($_GET['lon'])) {
             telegramApiLocation($PresseChatID, $PresseBotID, $threadID, $_GET['lat'], $_GET['lon'], $messageID);
         }
-
     } else {
         echo "Unbekannter API Key";
     }
